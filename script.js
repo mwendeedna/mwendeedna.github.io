079292982f0ae115eebe46b6c1d83d53919d7af2
mwendeedna <46905876+mwendeedna@users.noreply.github.com>
2026-07-04 00:12:52 +0000

    Initial Portfolio Website

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+window.addEventListener("scroll", () => {
 
+const navbar = document.querySelector(".navbar");
+
+if(window.scrollY > 50){
+    navbar.classList.add("shadow");
+}else{
+    navbar.classList.remove("shadow");
+}
+
+});
